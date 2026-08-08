3f3dd70ed7a18e93ef598ec309d20db8e343fa70
SaraAly28 <sara.aly28@proton.me>
2026-08-08 13:04:18 +0400

    Enhance Endometriosis analysis with SQL queries
    Added SQL comments and queries for dataset overview and patient count.

--- Endometriosis-sql-analysis/Endometriosis_analysis.sql
+++ Endometriosis-sql-analysis/Endometriosis_analysis.sql
@@ -1,1 +1,12 @@
+-- ============================================
+-- Endometriosis SQL Analysis
+-- Author: Sara Aly
+-- ============================================
 
+-- 1. View the dataset
+SELECT *
+FROM structured_endometriosis_data;
+
+-- 2. Count total patients
+SELECT COUNT(*) AS total_patients
+FROM structured_endometriosis_data;
